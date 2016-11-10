ab2fa493c8084a37522bcbff030746df30327a0f
User <User@DX3NBB2>
2016-11-10 09:31:56 +0100

    Test

---
+++ LendingLibrary/src/ui/Main.java
@@ -0,0 +1,51 @@
+package ui;
+
+import models.Book;
+import models.BookCatalog;
+import models.Customer;
+import models.DVD;
+import utilities.GenderType;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		BookCatalog bookCatalog = new BookCatalog();
+
+		Book book1 = new Book(1, "Introduction to Java", "Matt Greencroft", "12345", "Anytown Branch", 400);
+		Book book2 = new Book(2, "Better Java", "Joe Le Blanc", "23456", "Anytown Branch", 150);
+		DVD dvd1 = new DVD(3, "Epic Film", "Anytown Branch", "Stephen Spielberg", "99887", 120);
+
+		System.out.println(dvd1.getTitle());
+		book1.relocate("My City Branch");
+
+		bookCatalog.addBook(book1);
+		bookCatalog.addBook(book2);
+
+		UI ui = new UI();
+		ui.printHeader();
+
+		ui.printBookCatalog(bookCatalog.getBookArray());
+
+		Book foundBook = bookCatalog.findBook("Better java");
+		if (foundBook != null) {
+			System.out.println("We found " + foundBook.getTitle());
+		}
+
+		Customer customer = new Customer("Mr", "Mike", "Smith", "1 High St.", "1234456", "m@s.com", 1, GenderType.MALE);
+		System.out.println(customer.getExpiryDate());
+		System.out.println(customer.getMailingName());
+
+		System.out.println(dvd1.lend(customer));
+		dvd1.licence();
+		System.out.println(dvd1.lend(customer));
+		System.out.println(dvd1.lend(customer));
+		
+		System.out.println(book1.lend(customer));
+		System.out.println(book1.lend(customer));
+		
+		System.out.println(book1.getLoanPeriod());
+		System.out.println(dvd1.getLoanPeriod());
+	}
+
+}
