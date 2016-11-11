4101bba84e89f0d727023ccc897885e65d4966f1
User <User@DX3NBB2>
2016-11-11 16:42:15 +0100

    Update

--- Employee Management System/src/MainEMS.java
+++ Employee Management System/src/MainEMS.java
@@ -4,51 +4,24 @@ import java.util.Scanner;
 public class MainEMS {
 
 	public static void main(String[] args) {
+	
+		UserInput.firstChoice();
 		
 		ArrayList<Employee> employeeCatalog = new ArrayList<Employee>();
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("What would you like to do?");
-		System.out.println("1. Employee Management");
-		System.out.println("2. Employee Statistics");
-		System.out.println("3. Exit");
-		
-		int choice = scanner.nextInt();
-		
-		switch (choice) {
-		case 1:
-			System.out.println("What would you like to do?");
-			System.out.println("1. Add employee");
-			System.out.println("2. Delete employee");
-			System.out.println("3. Update name of employee");
-			System.out.println("4. Exit");
-			break;
-		case 2:
-			System.out.println("What would you like to do?");
-			System.out.println("1. Calculate bonus for employee");
-			System.out.println("2. Calculate average salary");
-			System.out.println("3. Calculate total bonus");
-			System.out.println("4. Exit");
-			break;
-		case 3:
-			System.out.println("Goodbye!");		
-			break;
-		case 4:
-			System.out.println("Invalid choice. Try again!");
-		}
+					
 	
-		Technician technician1 = new Technician("Pin Jim", "29 May 1970", 33000, "female");
-		Technician technician2 = new Technician("Lin Jen", "23 April 1991", 29000, "female");
-		Administrator administrator1 = new Administrator("Tom Boll", "12 January 1979", 25000, "male");
-		
-		employeeCatalog.add(technician1);
-		employeeCatalog.add(technician2);
-		employeeCatalog.add(administrator1);
-		
-		System.out.println(employeeCatalog.size());
-		
-		System.out.println(technician1.getFullName() + " will get " + technician1.calculateBonus(technician1.getSalary()) + " kr in bonus.");
-		System.out.println(administrator1.getFullName() + " is employee number " + administrator1.getId());
+//		Technician technician1 = new Technician("Pin Jim", "29 May 1970", 33000, "female");
+//		Technician technician2 = new Technician("Lin Jen", "23 April 1991", 29000, "female");
+//		Administrator administrator1 = new Administrator("Tom Boll", "12 January 1979", 25000, "male");
+//		
+//		employeeCatalog.add(technician1);
+//		employeeCatalog.add(technician2);
+//		employeeCatalog.add(administrator1);
+//		
+//		System.out.println(employeeCatalog.size());
+//		
+//		System.out.println(technician1.getFullName() + " will get " + technician1.calculateBonus(technician1.getSalary()) + " kr in bonus.");
+//		System.out.println(administrator1.getFullName() + " is employee number " + administrator1.getId());
 	}
 
 }
---
+++ Employee Management System/src/UserInput.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class UserInput {
+
+	static Scanner scanner = new Scanner(System.in);
+	
+	public static void firstChoice() {
+		System.out.println("What would you like to do?");
+		System.out.println("1. Employee Management");
+		System.out.println("2. Employee Statistics");
+		System.out.println("3. Exit");
+		
+		int choice = scanner.nextInt();
+		
+		switch (choice) {
+		case 1:
+			System.out.println("What would you like to do?");
+			System.out.println("1. Add employee");
+			System.out.println("2. Delete employee");
+			System.out.println("3. Update name of employee");
+			System.out.println("4. Back to Main Menu");
+			int choice2 = scanner.nextInt();
+			
+			switch (choice2) {
+		case 1: 
+			System.out.println("Adding employee");
+			break;
+		case 2:
+			System.out.println("Deleting employee");
+			break;
+		case 3:
+			System.out.println("Updating name of employee");
+			break;
+		case 4:
+			System.out.println("Returning to Main Menu");
+		default:
+			System.out.println("Invalid input");
+			}
+			
+			break;
+			
+		case 2:
+			System.out.println("What would you like to do?");
+			System.out.println("1. Calculate bonus for employee");
+			System.out.println("2. Calculate average salary");
+			System.out.println("3. Calculate total bonus");
+			System.out.println("4. Return to Main Menu");
+			break;
+		case 3:
+			System.out.println("Goodbye!");		
+			break;
+		case 4:
+			System.out.println("Invalid choice. Try again!");
+		}
+	}
+}
